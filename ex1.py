2bd6df1f5d11afa1ff7940a49c1523f4b68a59cc
donbeto <aaronalbertotb@gmail.com>
2021-10-21 14:11:17 -0500

    assert statements added

--- ex1.py
+++ ex1.py
@@ -10,12 +10,19 @@ Guaranteed constraints:
 
 """
 def largest_product(list_nums):
-    return max(a*b for a, b in zip(list_nums, list_nums[1:]))
+    try:
+        if len(list_nums) < 2 or len(list_nums) > 10:
+            raise ValueError("You just can enter a list larger than lenght 2 or smaller than lenght 10")
+        return max(a*b for a, b in zip(list_nums, list_nums[1:]))
+    except ValueError as ve:
+        return print(ve)
 
 
 def run():
     lenght_list = int(input("What's the list's lenght you want to generate?: "))
     given = [random.randint(-1000, 1000) for i in range(lenght_list)]
+    # this is another way to handle the lenght constraint
+    # assert lenght_list >= 2 and lenght_list <= 10, "You just can enter a list larger than lenght 2 or smaller than lenght 10"
     print(f"Random array: {given}")
     prod = largest_product(given)
     print(f'The largest product in the array is: {prod}')
---
+++ ex2.py
@@ -0,0 +1,19 @@
+"""
+Given a sequence of integers as an array, determine whether it is possible to obtain
+ a strictly increasing sequence by removing no more than one element from the array.
+ 2 ≤ sequence.length ≤ 105,
+-105 ≤ sequence[i] ≤ 105.
+	- [output] boolean
+	- Return true if it is possible to remove one element from the array in order to get 
+        a strictly increasing sequence, otherwise return false.
+
+"""
+
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
